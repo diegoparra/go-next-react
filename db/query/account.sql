7bca56d959d76b3753ffe39ba4bbf500de3de2fe
Diego Parra <diegoparraferreira@gmail.com>
2023-04-12 15:11:06 -0400

    feat: updates

---
+++ db/query/account.sql
@@ -0,0 +1,33 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  user_id,
+  category_id,
+  title,
+  type,
+  description
+  value,
+  date
+) VALUES (
+  $1, $2, $3, $4, $5, $6, $7
+) RETURNING *;
+
+
+-- name: GetAccount :one
+SELECT * FROM accounts 
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAccounts :many
+SELECT * FROM accounts a
+LEFT JOIN categories c ON c.id = a.category_id
+WHERE a.user_id = $1
+AND a.type = $2
+AND a.category_id = $3
+AND a.title like $4
+AND a.description like $5
+AND a.date = $6;
+
+-- name: UpdateAccount :one
+UPDATE accounts SET title = $2, description = $3 WHERE id = $1 RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diegoparra/go-next-react
+
+go 1.19
